2088e1e4e9a03ddd8e8c87203be1d2fb427ee627
Millrocious <vitalii.merdak.20@pnu.edu.ua>
2023-10-24 20:57:29 +0300

    - Refactor JwtAuthFilter and add extractRole method in JwtService

--- src/main/java/co/inventorsoft/academy/musicmanager/security/JwtAuthFilter.java
+++ src/main/java/co/inventorsoft/academy/musicmanager/security/JwtAuthFilter.java
@@ -10,20 +10,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
 @RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
-    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(
@@ -34,6 +32,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
+        final String userRole;
 
         try {
             if (authHeader == null || !authHeader.startsWith("Bearer ")) {
@@ -42,24 +41,21 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             }
             jwt = authHeader.substring(7);
             userEmail = jwtService.extractUsername(jwt);
-            if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-                if (jwtService.isTokenValid(jwt)) {
-                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                            userDetails,
-                            null,
-                            userDetails.getAuthorities()
-                    );
-                    authToken.setDetails(
-                            new WebAuthenticationDetailsSource().buildDetails(request)
-                    );
-                    SecurityContextHolder.getContext().setAuthentication(authToken);
-                }
+            userRole = jwtService.extractRole(jwt);
+            if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null && (jwtService.isTokenValid(jwt))) {
+                SecurityContextHolder.getContext().setAuthentication(
+                        new UsernamePasswordAuthenticationToken(
+                                userEmail,
+                                null,
+                                Collections.singleton(new SimpleGrantedAuthority(userRole))
+                        )
+                );
             }
         } catch (ExpiredJwtException e) {
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
             response.getWriter().write(e.getMessage());
             response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            filterChain.doFilter(request, response);
             return;
         }
         filterChain.doFilter(request, response);
